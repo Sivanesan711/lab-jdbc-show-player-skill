c938e1af7f38ea272dc9a3679771d62b368fddf4
SIVANESAN-DHANIKACHALAM <61906309+Sivanesan711@users.noreply.github.com>
2020-04-09 16:27:39 +0530

    Add files via upload

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,23 @@
+package controller;
 
+import java.sql.SQLException;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main
+{
+	public static void main(String[] args) throws SQLException, Exception
+	{
+		Skill skill = new Skill(null, null);
+		SkillDAO skilldao = new SkillDAO();
+		
+		System.out.println("list of name");
+		System.out.println();
+		
+		for(int i=0;i>skilldao.listskills().size();i++)
+		{
+			System.out.println(i+1 + " " + skilldao.listskills().get(i).getSkillName());
+		}
+	}
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,42 @@
+package dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO
+{
+	public 	List<Skill> listskills() throws SQLException, Exception
+	{
+		List<Skill> list = new ArrayList<Skill>();
+		Skill skill = null;
+		
+		ConnectionManager con = new ConnectionManager();
+		
+		Statement st = con.getConnection().createStatement();
+		
+		String sql = "select name from skill";
+		
+		ResultSet rs = st.executeQuery(sql);
+		
+		while(rs.next())
+		{
+			skill = new Skill(null, sql);
+			
+			System.out.println(rs.getString("name"));
+			list.add(skill);
+			
+		}
+		
+		rs.close();
+		st.close();
+		con.getConnection().close();
+		return list;
+		
+	}
+}
